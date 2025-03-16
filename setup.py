e3bd7367b80e48a182eefa1d327781a51ef74ab4
abhichris63 <itsabhichrishere@gmail.com>
2025-03-16 22:47:08 +0530

    Logging and Exception

--- setup.py
+++ setup.py
@@ -3,19 +3,31 @@ from typing import List
 
 HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str) -> List[str]:
-    '''
-    This function will return the list of requirements
-    '''
+    """
+    This function reads the requirements file and returns a list of requirements.
+
+    Args:
+        file_path (str): The path to the requirements.txt file.
+
+    Returns:
+        List[str]: A list of package requirements excluding '-e .'
+    """
 
     requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n"," ") for req in requirements]
+    try:
+        with open(file_path) as file_obj:
+            requirements = file_obj.readlines()
+            requirements = [req.replace("\n"," ") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+
     return requirements
 
+
+# Package setup configuration
 setup(
     name = 'MLproject',
     version = '0.0.1',
---
+++ src/exception.py
@@ -0,0 +1,35 @@
+import sys
+import logging
+
+def error_message_details(error, error_detail:sys):
+    """
+    This function captures detailed error information including:
+    - The file name where the error occurred.
+    - The line number where the error occurred.
+    - The error message itself.
+    """
+    
+    _,_,exc_tb = error_detail.exc_info()          # exc_tb: returns in which file & line the error has occured.
+    file_name = exc_tb.tb_frame.f_code.co_filename # Getting the file name
+    line_number = exc_tb.tb_lineno                 # Getting the line number where error occurred
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, line_number, str(error)
+    )
+
+    return error_message
+
+# Customized Exception Handling class inheriting from base Exception class
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        """
+        Initializes the CustomException with detailed error information.
+        """
+
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_detail = error_detail)
+
+    def __str__(self):
+        """
+        Returns the error message as a string representation of the object.
+        """
+        return self.error_message
